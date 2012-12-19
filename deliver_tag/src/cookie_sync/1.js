eb1534cca5271804c9f29196453fa54dc6b2bc66
ynomura1941 <Yoshihiro_Nomura@voyagegroup.com>
2012-12-19 10:44:46 +0900

    add comment

--- deliver_tag/src/cookie_sync/1.js
+++ deliver_tag/src/cookie_sync/1.js
@@ -4,6 +4,9 @@
 if (typeof (window['adingoFluctSync']) === 'undefined') {
   var AdingoFluctSync = function () {};
 
+  /**
+   * logly 用 cookie sync tag 生成
+   */
   AdingoFluctSync['logly'] = function (util, logyid) {
     var ref = '', url = window.document.referrer;
     try {
@@ -24,6 +27,9 @@ if (typeof (window['adingoFluctSync']) === 'undefined') {
     return util.beacon(src);
   };
 
+  /**
+   * scaleout 用 cookie sync tag 生成
+   */
   AdingoFluctSync['scaleout'] = function (util) {
     var so_tp = encodeURIComponent(window.document.location.href);
     var so_pp = encodeURIComponent(window.document.referrer);
@@ -31,10 +37,20 @@ if (typeof (window['adingoFluctSync']) === 'undefined') {
     '&tp=' + so_tp + '&pp=' + so_pp + "&t=.gif";
     return util.beacon(so_src);
   };
+  
+  /**
+   * freakout 用 cookie sync tag 生成
+   */
   AdingoFluctSync['fout'] = function (util) {
     return util.beacon('http://sync.fout.jp/sync?xid=fluct');
   };
 
+  /**
+   * response jsonのsyncsの内容に従って、cookie sync tag をレンダリングする
+   * @param util ユーティリティーオブジェクト
+   * @param target レンダリングの基準となるelement
+   * @param syncs response json のsyncs
+   */
   AdingoFluctSync.render = function (util, target, syncs) {
     for (var sync in syncs) {
       if (sync === 'logly') {
